897a6a8aa1b38081e4dc6b63e3cb24b839cff8a4
saravanansivardm <sivasaravananrdm@gmail.com>
2025-03-29 10:37:16 +0530

    Added updated library versions.

--- app/src/main/java/com/example/cryptocurrencyapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/cryptocurrencyapp/presentation/MainActivity.kt
@@ -4,13 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.cryptocurrencyapp.presentation.theme.CryptocurrencyAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +26,37 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CryptocurrencyAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(Color.Cyan),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
                     Greeting(
-                        name = "AndroiDDDDd",
-                        modifier = Modifier.padding(innerPadding)
+                        name = "Welcome Modified gradle AndroiDDDDd",
+                        modifier = Modifier.padding(16.dp)
                     )
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    @Composable
+    fun Greeting(name: String, modifier: Modifier = Modifier) {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier.fillMaxWidth(),
+            color = Color.Blue,
+            fontWeight = FontWeight.Bold
+        )
+    }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CryptocurrencyAppTheme {
-        Greeting("Android")
+    @Preview(showBackground = true)
+    @Composable
+    fun GreetingPreview() {
+        CryptocurrencyAppTheme {
+            Greeting("Android")
+        }
     }
 }
