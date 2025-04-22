61d67abfc907f21108c691a59ca7fbb0884276ae
saravanansivardm <sivasaravananrdm@gmail.com>
2025-04-22 09:46:50 +0530

    Removed PMD files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,6 @@ plugins {
     id("dagger.hilt.android.plugin")
     kotlin("kapt")
     id("io.gitlab.arturbosch.detekt")
-    id("pmd")
 }
 
 android {
@@ -68,43 +67,6 @@ android {
     }
 
 }
-
-// PMD Configuration (outside android block)
-pmd {
-    toolVersion = "6.55.0"
-    isConsoleOutput = true
-    ruleSetFiles = files("config/pmd/ruleset.xml")
-    ruleSets = listOf()
-}
-
-
-tasks.register<Pmd>("runPmd") {
-    group = "verification"
-    description = "Run PMD analysis"
-
-    source = fileTree("src/main/java") {
-        include("**/*.java")
-        exclude(
-            "**/test/**",
-            "**/androidTest/**",
-            "**/commonTest/**",
-            "**/jvmTest/**",
-            "**/androidUnitTest/**",
-            "**/androidInstrumentedTest/**",
-            "**/jsTest/**",
-            "**/iosTest/**"
-        )
-    }
-
-    ruleSetFiles = files("${rootDir}/config/pmd/ruleset.xml")
-    ruleSets = listOf()
-
-    reports {
-        xml.required.set(true)
-        html.required.set(true)
-    }
-}
-
 dependencies {
     implementation(libs.androidx.activity.ktx)
     implementation(libs.androidx.core.ktx.v1150)
